b6e3ffcec4e011a0ac97745391434a637218595c
AngelLopez03 <angellopezalarcon832@gmail.com>
2025-01-06 17:51:50 -0300

    Componente de formulario optimizado

--- form-component/src/main.tsx
+++ form-component/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import FormComponent from './App.tsx'
+import FormComponent from './Form.tsx'
 import { Inputs } from './types'
 
 const inputs:Array<Inputs> = [
