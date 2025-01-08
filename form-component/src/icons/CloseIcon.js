2f77209dfea0cc851f5bd8e27838993c1ea7cb21
AngelLopez03 <angellopezalarcon832@gmail.com>
2025-01-08 16:23:19 -0300

    Agregamos iconos

---
+++ form-component/src/icons/CloseIcon.js
@@ -0,0 +1,4 @@
+import { jsx as _jsx } from "react/jsx-runtime";
+export const CloseIcon = (props) => {
+    return (_jsx("svg", { xmlns: "http://www.w3.org/2000/svg", viewBox: "0 -960 960 960", ...props, children: _jsx("path", { d: "m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" }) }));
+};
---
+++ form-component/src/icons/VisibilityIcon.d.ts
@@ -0,0 +1,4 @@
+import React from "react";
+import { IconProps } from "../types";
+export declare const VisibilityIcon: React.FC<IconProps>;
+export declare const VisibilityIconOff: React.FC<IconProps>;
