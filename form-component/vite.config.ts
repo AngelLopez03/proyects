d38d2558c33a0e4b02798234fd83c71ca1b2f4e5
AngelLopez03 <angellopezalarcon832@gmail.com>
2025-01-08 16:24:26 -0300

    Configuraciones de compilación

--- form-component/vite.config.ts
+++ form-component/vite.config.ts
@@ -1,7 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from "path";
+import dts from "vite-plugin-dts";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'index.ts'),
+      name: 'FormComponent',
+      fileName: (format) => `index.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom', 'classnames'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+          classnames: 'classNames'
+        },
+      },
+    },
+
+    sourcemap: true,
+    emptyOutDir: true,
+  },
+  plugins: [react(), dts()],
+});
